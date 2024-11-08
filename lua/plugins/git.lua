c5698736c5afe58fb0afe94e050dbc09ea02b0db
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-11-08 19:02:13 +0200

    barbar,which-key edits

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,10 @@
 -- nvim v0.8.0
 return {
+  -- installed for barbar, gutter marks on git status, or something --
+  {
+      "lewis6991/gitsigns.nvim",
+      opts = {},
+  },
   -- optional for lazygit floating window border decoration
   { "nvim-lua/plenary.nvim", lazy=true},
   {
@@ -15,6 +20,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
+        { "<leader>l", desc="git" },
         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
 },
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,18 +1,42 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+  {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
   },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+  {'romgrk/barbar.nvim',
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
     },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+
+
+  {
+  "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  }
+
+
 }
